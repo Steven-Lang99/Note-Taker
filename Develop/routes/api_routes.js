43e2cf65f3b55d1ad97fe9e78721e279da3cd3c5
Steve Lang <langloiss0003@my.uwstout.edu>
2022-07-27 17:21:01 -0500

    note taker working

--- Develop/routes/api_routes.js
+++ Develop/routes/api_routes.js
@@ -1,6 +1,7 @@
 const note_router = require('express').Router();
 const fs = require('fs');
 const path = require('path')
+const uuid = require('uuid').v4
 const db = path.join(__dirname, '../db/db.json')
 
 function getNoteData() {
@@ -20,7 +21,26 @@ note_router.get('/notes', (request, response) => {
 })
 
 
+note_router.post('/notes', (request, response) => {
+    getNoteData()
+        .then(note_data => {
+            const note = request.body
+            note.id = uuid().split(0, 1)
 
+
+            note_data.push(note)
+
+
+            fs.promises.writeFile(db, JSON.stringify(note_data))
+                .then((err) => {
+                    console.log('todo updated')
+
+                })
+                .catch(err => console.log(err));
+            response.json(note)
+        });
+
+})
 
 
 
